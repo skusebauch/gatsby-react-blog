9dfd0bf0c1dfa4de2b1283b896b97d235d8ae154
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-10-06 18:11:13 +0200

    add hook with query to use site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,30 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Gatsby React Blog`,
+    description: `This is a my coding blog developed by Sven`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import { useSiteMetadata } from "../hooks/useSiteMetadata"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const { title, description } = useSiteMetadata()
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </div>
+  )
 }
