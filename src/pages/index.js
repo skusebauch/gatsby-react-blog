977ccee49f2bac102a5cdc6f85e8ebecef6794d9
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-10-07 10:46:04 +0200

    add styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from "react"
 import { useSiteMetadata } from "../hooks/useSiteMetadata"
+import styled from "styled-components"
+
+const StyledH1 = styled.h1`
+  color: rebeccapurple;
+`
 
 export default function Home() {
   const { title, description } = useSiteMetadata()
   return (
     <div>
-      <h1>{title}</h1>
+      <StyledH1>{title}</StyledH1>
       <p>{description}</p>
     </div>
   )
